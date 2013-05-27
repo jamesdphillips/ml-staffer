834403e7f93c8312fdf1f6657d35624761e3a60d
James Phillips <jamesdphillips@gmail.com>
2013-05-27 13:20:54 -0700

    Base rails app
    * Rails 4.0 RC1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MlStaffer::Application.config.secret_key_base = '2351e447f3e88bd5d27616dc7976d54306ae288f4a5d64a09177dc7e80df7e8d3ec842ac3bd8a1018943791ba72643a2dfa4be92e2feb86c4b7ae70c974eb07d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MlStaffer::Application.config.session_store :cookie_store, key: '_ml-staffer_session'
