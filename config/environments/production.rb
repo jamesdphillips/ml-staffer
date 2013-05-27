a4c45441e1d094da2ff59bec87f0e95a8ef5fac0
Billy He <qiushihe@me.com>
2013-05-27 14:31:41 -0700

    Add user seeds and directoy index template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ MlStaffer::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => ENV["ML_STAFFER_DOMAIN"] || "localhost:3000" }
+
   # Enable threaded mode
   # config.threadsafe!
 
