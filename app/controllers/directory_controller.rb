4ae7fe89d66bd3bdb6f674db2177c65d9e158b84
Billy He <qiushihe@me.com>
2013-05-27 18:28:08 -0700

    Update all users facebook checkin

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -3,7 +3,7 @@ class DirectoryController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    current_user.update_facebook_checkin
+    User.all.map(&:update_facebook_checkin)
     @users = User.all
   end
 
